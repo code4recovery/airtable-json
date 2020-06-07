401575e470404c0b71e15978fa7dab617d3c31ce
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2020-06-07 13:08:44 -0700

    fixing days

--- app/Http/Controllers/FormatController.php
+++ app/Http/Controllers/FormatController.php
@@ -10,15 +10,21 @@ class FormatController extends Controller
     static function convert($rows) {
         $meetings = [];
 
+        //temporary lookup for days
+        $days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
         foreach ($rows as $row) {
-            if (empty($row->fields->{"Locations_Street_Address"}[0])) {
+
+            //some rows are empty, here's a simple hack
+            if (empty($row->fields->{"Locations_Site_Name"}) || !in_array($row->fields->{"Day"}, $days)) {
                 continue;
             }
+
             $meetings[] = [
                 'slug' => $row->id,
                 'types' => array_map(function($type) { return strtoupper(trim($type, " \t\n\r\0\x0B\xc2\xa0")); }, $row->fields->{"Designations"}),
                 'name' => $row->fields->{"Meeting Name [Legacy]"},
-                'day' => $row->fields->{"Day"},
+                'day' => array_search($row->fields->{"Day"}, $days),
                 'time' => date('H:i', strtotime($row->fields->{"Start Time"})),
                 'notes' => $row->fields->{"Meeting Note"},
                 'location' => $row->fields->{"Locations_Site_Name"}[0],
